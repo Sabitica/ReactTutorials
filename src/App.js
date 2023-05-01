18ed1e894003b453946885045d4174f796a88001
Sabitica <sarahlecornec123+1@gmail.com>
2023-05-01 16:20:01 +0100

    install react-router-dom for tutorial pages

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
 import React, { Component } from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './App.css';
+import TwoWayBinding from './Components/TwoWayBinding';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/two-way-binding",
+    element: <TwoWayBinding />
+  }
+]);
 
 class App extends Component {
-  state = {
-    name: 'Sabitica'
-  }
-
-  changeName = (newName) => {
-    this.setState({
-      name: newName
-    })
-  }
-
-  changeNameFromInput = (event) => {
-    this.setState({
-      name: event.target.value
-    })
-  }
 
   render() {
     return (
       <div className="App">
-        <br /><br />
-        <button onClick={this.changeName.bind(this, 'Sarah Jane')}>Change State Using Bind</button>
-        <button onClick={() => this.changeName('Sarah')}>Change State</button>
-        <br /><br />
-        <input type="text" onChange={this.changeNameFromInput} value={this.state.name}/>
-        <div>{this.state.name}</div>
+        <RouterProvider router={router} />
       </div>
     )
   }
