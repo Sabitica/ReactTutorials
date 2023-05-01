3017eb47fc6c28283b10f87926b54c0f3b8e8190
Sabitica <sarahlecornec123+1@gmail.com>
2023-05-01 16:09:25 +0100

    create react tutorials project, tutorial on react events and two way binding

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import './App.css';
+
+class App extends Component {
+  state = {
+    name: 'Sabitica'
+  }
+
+  changeName = (newName) => {
+    this.setState({
+      name: newName
+    })
+  }
+
+  changeNameFromInput = (event) => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <br /><br />
+        <button onClick={this.changeName.bind(this, 'Sarah Jane')}>Change State Using Bind</button>
+        <button onClick={() => this.changeName('Sarah')}>Change State</button>
+        <br /><br />
+        <input type="text" onChange={this.changeNameFromInput} value={this.state.name}/>
+        <div>{this.state.name}</div>
+      </div>
+    )
+  }
+}
+
+export default App;
